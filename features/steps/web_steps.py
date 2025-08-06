1f51a726d17511a4a886a0d6a3a65eed070bfa07
abdyc <abdylla.ch@gmail.com>
2025-08-06 10:39:57 -0400

    feat: complete all TDD and BDD exercises

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -43,12 +43,12 @@ def step_impl(context):
 @then('I should see "{message}" in the title')
 def step_impl(context, message):
     """ Check the document title for a message """
-    assert(message in context.driver.title)
+    assert message in context.driver.title
 
 @then('I should not see "{text_string}"')
 def step_impl(context, text_string):
     element = context.driver.find_element(By.TAG_NAME, 'body')
-    assert(text_string not in element.text)
+    assert text_string not in element.text
 
 @when('I set the "{element_name}" to "{text_string}"')
 def step_impl(context, element_name, text_string):
@@ -67,13 +67,13 @@ def step_impl(context, text, element_name):
 def step_impl(context, text, element_name):
     element_id = ID_PREFIX + element_name.lower().replace(' ', '_')
     element = Select(context.driver.find_element(By.ID, element_id))
-    assert(element.first_selected_option.text == text)
+    assert element.first_selected_option.text == text
 
 @then('the "{element_name}" field should be empty')
 def step_impl(context, element_name):
     element_id = ID_PREFIX + element_name.lower().replace(' ', '_')
     element = context.driver.find_element(By.ID, element_id)
-    assert(element.get_attribute('value') == u'')
+    assert element.get_attribute('value') == ''
 
 ##################################################################
 # These two function simulate copy and paste
@@ -104,7 +104,35 @@ def step_impl(context, element_name):
 # to get the element id of any button
 ##################################################################
 
-## UPDATE CODE HERE ##
+@when('I press the "{button}" button')
+def step_impl(context, button):
+    button_id = button.lower().replace(' ', '_') + '-btn'
+    context.driver.find_element(By.ID, button_id).click()
+
+@then('I should see "{name}" in the results')
+def step_impl(context, name):
+    found = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'search_results'),
+            name
+        )
+    )
+    assert found
+
+@then('I should not see "{name}" in the results')
+def step_impl(context, name):
+    element = context.driver.find_element(By.ID, 'search_results')
+    assert name not in element.text
+
+@then('I should see the message "{message}"')
+def step_impl(context, message):
+    found = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'flash_message'),
+            message
+        )
+    )
+    assert found
 
 ##################################################################
 # This code works because of the following naming convention:
@@ -122,7 +150,7 @@ def step_impl(context, text_string, element_name):
             text_string
         )
     )
-    assert(found)
+    assert found
 
 @when('I change "{element_name}" to "{text_string}"')
 def step_impl(context, element_name, text_string):
